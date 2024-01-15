7c6bffcd0fee4bc550adf0dbb5b887221fdf5972
Huian-Yang <109931665+Huian-Yang@users.noreply.github.com>
2024-01-15 00:30:55 -0800

    Add files via upload

---
+++ Clock Program.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from time import * 
+
+def update():
+    time_string = strftime("%I:%M:%S %p") #display current time
+    time_label.config(text=time_string) #updates time label
+    
+    day_string = strftime("%A") #display day of the week
+    day_label.config(text=day_string) #updates time
+    
+    date_string = strftime("%B %d, %Y") #display month,day,year
+    date_label.config(text=date_string) #updates time
+    
+    window.after(1000,update)   #update this window every second/1000 milisecond
+                                #recursive function, calling a function in a function
+
+window = Tk()
+
+time_label = Label(window,font=("Arial",50),fg="green",bg="black")
+time_label.pack()
+
+day_label = Label(window,font=("Ink Free",25))
+day_label.pack()
+
+date_label = Label(window,font=("Ink Free",30))
+date_label.pack()
+
+update() #recursive function
+
+
+window.mainloop()
